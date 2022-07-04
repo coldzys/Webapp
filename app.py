fc7bc53ca854c91cb1ca80a846d554057de0f851
Quân Ng <54743604+coldzys@users.noreply.github.com>
2022-07-04 13:52:47 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,32 @@
 import streamlit as st
 
-st.write("Webapp")
+
+# site settings
+st.set_page_config(layout="wide")
+
+# sidebar settings
+with st.sidebar.form(key='my_form'):
+  st.subheader('Glass composition')
+
+  st.slider("SiO\u2082 concentration, mol%",
+                  min_value = 50.0,
+                  max_value = 100.0,
+                  key='x1')
+
+  st.slider("Al\u2082O\u2083 concentration, mol%",
+                  min_value = 0.0,
+                  max_value = 50.0,
+                  key='x2')
+
+  st.slider("Na\u2082O concentration, mol%",
+                  min_value = 0.0,
+                  max_value = 50.0,
+                  key='x3')
+
+  st.slider("K\u2082O concentration, mol%",
+                  min_value = 0.0,
+                  max_value = 50.0,
+                  key='x4')
+
+  submit_button = st.form_submit_button(label='Calculate!', on_click=form_callback)
+  st.write("When you run the model, compositions will be rescaled to ensure they sum to 100%.")
